77e7addfecacffe81238d438317158799ac619a0
maryam-m010 <mmaryam7@outlook.com>
2025-03-13 18:00:59 +0000

    Update calendar_proj.py

--- calendar_proj.py
+++ calendar_proj.py
@@ -2,7 +2,14 @@ from tkinter import *
 import calendar
 def show_cal():
     new_root = Tk()
-    new_root.config("white")
+    new_root.config(background = "white")
+    new_root.title("calendar")
+    new_root.geometry("550x600")
+    z = int(b.get())
+    r = calendar.calendar(z)
+    m = Label(new_root, text = r, font = "Consolas 10 bold")
+    m.grid(row = 5, column = 1, padx = 20)
+    new_root.mainloop()
 
 
 if __name__ == "__main__":
@@ -19,10 +26,11 @@ if __name__ == "__main__":
     b = Entry(root, width = 30)
     b.grid(row = 2, column = 2)
 
-    s = Button(root, text = "Show Calendar", fg = "black", bg = "white")
+    s = Button(root, text = "Show Calendar", fg = "black", bg = "white", command = show_cal)
     s.grid(row = 3, column = 2)
 
-    m = Button(root, text = "Exit", fg = "black", bg = "white")
+    m = Button(root, text = "Exit", fg = "black", bg = "white", command = exit)
     m.grid(row = 4, column = 2)
 
     root.mainloop()
+
